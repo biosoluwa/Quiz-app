6c466a869e4933a6b6b49eb89adeb7dfe8e79928
biosoluwa <favbiose@gmail.com>
2026-02-07 23:58:47 +0100

    javascript updates

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ import quizData from "./quizData.js";
 let render = document.getElementById('render')
 let formElm = document.getElementById('form-el')
 let currentQuestionIndex = 0
+let trackScore = 0
 
 document.addEventListener('click', function(e){
     if(e.target.id === 'submit'){
         checkAnswer()
     }else if(e.target.id === 'restart'){
-        currentQuestionIndex = 0
-        renderQuestion() 
-        document.getElementById('display-score').innerHTML = '' 
- }
+      restartQuiz()
+    }else if(e.target.id === 'start-quiz'){
+        startQuiz()
+    }
 })
+
+function startQuiz(){
+   renderQuestion()
+   document.getElementById('start-quiz').remove()
+}
 
 function renderQuestion(){
     let optionList = ''
@@ -27,9 +33,6 @@ function renderQuestion(){
    `
 }
 
-renderQuestion()
-
-    let trackScore = 0
 function checkAnswer(){
     let currentQuestion = quizData[currentQuestionIndex]
     const radioArr = document.querySelectorAll('input[name = "choice"]')
@@ -37,6 +40,7 @@ function checkAnswer(){
     const checkedRadio = Array.from(radioArr).find(function(radio){
           return radio.checked
     })
+    if(checkedRadio){
     if(currentQuestion.options.indexOf(checkedRadio.id) === currentQuestion.correct){
         trackScore++
         }           currentQuestionIndex++ 
@@ -46,10 +50,18 @@ function checkAnswer(){
         renderQuestion()
     }
 }
+}
 
 function showResult(){
     render.innerHTML = ''
     formElm.innerHTML = ''
-    document.getElementById('display-score').innerHTML = `<p>Your score: ${trackScore} out of ${quizData.length}</p>
+    document.getElementById('display-score').innerHTML = `<p>Your score: ${trackScore} out of ${quizData.length} (${Math.round(trackScore/quizData.length *100)}%)</p>
                                                           <button id="restart">Restart Quiz</button>`
+}
+
+function restartQuiz(){
+    currentQuestionIndex = 0
+    trackScore = 0
+    renderQuestion() 
+    document.getElementById('display-score').innerHTML = '' 
 }
